5bd41e1d0f96ace355bd7f349533bf22aabfb04f
NicolaSpadari <spadari.nicola@gmail.com>
2022-04-14 09:58:46 +0200

    Update deps

--- components.d.ts
+++ components.d.ts
@@ -2,9 +2,11 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Navbar: typeof import('./src/components/navbar.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
     Test: typeof import('./src/components/test.vue')['default']
   }
 }
