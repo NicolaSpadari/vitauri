79437f17f64b2d6ef3866f25e8e00e43103b3773
NicolaSpadari <spadari.nicola@gmail.com>
2023-04-08 11:17:43 +0200

    Add try catch in notification

--- src/composables/useNotification.ts
+++ src/composables/useNotification.ts
@@ -1,16 +1,20 @@
 import type { Options } from "@tauri-apps/api/notification";
 
 const useNotification = async (options: Options): Promise<string> => {
-    if (await isPermissionGranted()) {
-        sendNotification(options);
-        return "success";
-    } else {
-        if (await requestPermission()) {
-            useNotification(options);
+    try {
+        if (await isPermissionGranted()) {
+            sendNotification(options);
+            return "success";
+        } else {
+            if (await requestPermission()) {
+                await useNotification(options);
+            }
+            return "error";
         }
+    } catch (e) {
+        console.error(e);
+        return "error";
     }
-
-    return "error";
 };
 
 export default useNotification;
